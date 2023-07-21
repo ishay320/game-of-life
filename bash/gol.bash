9bec515183977174d8d29a880d07e7222d53ed36
ishay320 <ishay320@hotmail.com>
2023-07-21 12:11:11 +0300

    fixed warnings

--- bash/gol.bash
+++ bash/gol.bash
@@ -11,14 +11,14 @@ clear_screen() {
 }
 
 print_board() {
-    board=($@)
-    for ((i = 0; i < $height; i++)); do
-        for ((j = 0; j < $width; j++)); do
+    board=("$@")
+    for ((i = 0; i < height; i++)); do
+        for ((j = 0; j < width; j++)); do
             case "${board[i * width + j]}" in
-            ${states[1]})
+            "${states[1]}")
                 echo -n "# "
                 ;;
-            ${states[0]})
+            "${states[0]}")
                 echo -n "  "
                 ;;
             *)
@@ -35,48 +35,48 @@ circular_arr() {
     declare -i column=$2
     declare -i height=$3
     declare -i width=$4
-    echo $(($(($(($row + $height)) % $height)) * $width + $(($(($column + $width)) % width))))
+    echo $(($(($((row + height)) % height)) * width + $(($((column + width)) % width))))
 }
 
-for ((i = 0; i < $height * $width; i++)); do
+for ((i = 0; i < height * width; i++)); do
     board_a[i]=${states[0]}
     board_b[i]=${states[0]}
 done
 
 # R-Pentomino
-board_a[$(((($height / 2) + 0) * $width + (($width / 2) + 1)))]=${states[1]}
-board_a[$(((($height / 2) + 0) * $width + (($width / 2) + 0)))]=${states[1]}
-board_a[$(((($height / 2) + 1) * $width + (($width / 2) + 0)))]=${states[1]}
-board_a[$(((($height / 2) + 2) * $width + (($width / 2) + 0)))]=${states[1]}
-board_a[$(((($height / 2) + 1) * $width + (($width / 2) - 1)))]=${states[1]}
+board_a[((height / 2) + 0) * width + ((width / 2) + 1)]=${states[1]}
+board_a[((height / 2) + 0) * width + ((width / 2) + 0)]=${states[1]}
+board_a[((height / 2) + 1) * width + ((width / 2) + 0)]=${states[1]}
+board_a[((height / 2) + 2) * width + ((width / 2) + 0)]=${states[1]}
+board_a[((height / 2) + 1) * width + ((width / 2) - 1)]=${states[1]}
 
 while true; do
-    for ((i = 0; i < $height; i++)); do
-        for ((j = 0; j < $width; j++)); do
+    for ((i = 0; i < height; i++)); do
+        for ((j = 0; j < width; j++)); do
 
             # neighbors
             count=0
             for ((row = i - 1; row < i + 2; row++)); do
                 for ((column = j - 1; column < j + 2; column++)); do
-                    if ! { [[ $row == $i ]] && [[ $column == $j ]]; } &&
+                    if ! { [[ $row == "$i" ]] && [[ $column == "$j" ]]; } &&
                         [[ ${board_a[$(circular_arr $row $column $height $width)]} -eq ${states[1]} ]]; then
                         ((count++))
                     fi
                 done
             done
-            if [[ ${board_a[$i * $width + $j]} == ${states[1]} ]] && { [[ 2 == $count ]] || [[ $count == 3 ]]; }; then
-                board_b[(($i * $width + $j))]=${states[1]}
+            if [[ ${board_a[$i * $width + $j]} == "${states[1]}" ]] && { [[ $count == 2 ]] || [[ $count == 3 ]]; }; then
+                board_b[i * width + j]=${states[1]}
             elif [[ $count == 3 ]]; then
-                board_b[(($i * $width + $j))]=${states[1]}
+                board_b[i * width + j]=${states[1]}
             else
-                board_b[(($i * $width + $j))]=${states[0]}
+                board_b[i * width + j]=${states[0]}
             fi
         done
     done
 
-    tmp=(${board_a[@]})
-    board_a=(${board_b[@]})
-    board_b=(${tmp[@]})
+    tmp=("${board_a[@]}")
+    board_a=("${board_b[@]}")
+    board_b=("${tmp[@]}")
 
     clear_screen
     print_board "${board_a[@]}"
